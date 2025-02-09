b5969e1db274a8038cdacb6cd1298a5d9d329392
Ivan Adamo <ivan.adamo.8@gmail.com>
2025-02-09 12:47:54 -0300

    lalala

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const mainContainer = document.querySelector('.main-container');
 const btnAgregarOP = document.querySelector('.btn-agregar-op');
 const inputArticulo = document.getElementById('articulo');
 const inputInicio = document.getElementById('inicio');
@@ -7,6 +8,8 @@ const inputLote = document.getElementById('lote');
 const btnGrabar = document.querySelector('.grabar');
 const divAgregarOP = document.querySelector('.agregar-op');
 const btnCerrar = document.querySelector('.cerrar');
+const tablaOP = document.querySelector('.tabla-op');
+
 
 btnAgregarOP.addEventListener('click', () => {
   divAgregarOP.classList.add('active');
@@ -17,7 +20,25 @@ btnCerrar.addEventListener('click', () => {
 });
 
 btnGrabar.addEventListener('click', () => {
-  console.log("grabar");
-  ;
+
+  const articulo = inputArticulo.value;
+  const inicio = inputInicio.value;
+  const cantidad = inputCantidad.value;
+  const partida = inputPartida.value;
+  const lote = inputLote.value;
+
+  const lineaOP = document.createElement('tr');
+  lineaOP.innerHTML = `
+    <td>${articulo}</td>
+    <td>${inicio}</td>
+    <td>${cantidad}</td>
+    <td>${partida}</td>
+    <td>${lote}</td>
+  `;
+  
+  tablaOP.appendChild(lineaOP);
+
+  divAgregarOP.classList.remove('active');
+  
 })
 
